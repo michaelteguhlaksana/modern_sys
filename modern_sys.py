c5252669d8ecf56e3a232a62d5496474c016d8a7
Michael Teguh Laksana <michaelteguhlaksana@gmail.com>
2022-06-08 13:44:30 +0800

    Added the HaarCascadeClassifier class

--- modern_sys.py
+++ modern_sys.py
@@ -5,6 +5,9 @@ from mpl_toolkits import mplot3d
 import math
 import serial
 import time
+import cv2
+import PySimpleGUI as sg
+import pandas as pd
 
 '''
 import logging
@@ -18,7 +21,7 @@ class Cam_Arm(object):
 	A Robot arm model for holding the camera for the object recognition
 
 	"""
-	def __init__(self, base_pos, base_orient, seg_lengths, range_trans, range_rot, tol = 0.01, cam_orient = 270, comm_line = None, baud_rate = None, hard_run = False):
+	def __init__(self, base_pos, base_orient, seg_lengths, range_trans, range_rot, tol = 0.01, cam_orient = 270, comm_line = None, baud_rate = None, cam_num = None, hard_run = False):
 		super(Cam_Arm, self).__init__()
 		self.home_base_pos = base_pos
 		self.home_base_orient = base_orient
@@ -26,6 +29,9 @@ class Cam_Arm(object):
 		self.range_trans = range_trans
 		self.range_rot = range_rot
 		self.tol = tol
+
+		self.cam_num = cam_num
+		self.cam = cv2.VideoCapture(self.cam_num)
 
 		self.hard_run = hard_run
 		self.baud_rate = baud_rate
@@ -297,6 +303,253 @@ class Cam_Arm(object):
 
 		self.cnc.write(command)
 		time.sleep(2)
+
+	def capture(self):
+		pass
+
+	def detect(self):
+		pass
+		
+
+class HaarCascadeClassifier(object):
+	""" 
+	A Haar Cascade Classifier UI for detecting devices.
+	Adapted from the PRIMITV system
+
+	"""
+	def __init__(self, mask_path, result_folder, vid_cap):
+		super(HaarCascadeClassifier, self).__init__()
+		self.cascade_device = cv2.CascadeClassifier(mask_path)
+		self.cam = vid_cap
+		self.result_folder = result_folder
+		self.calibUnit = calibUnit
+
+		self.probe_pos = probe_pos
+		self.cam_pos = cam_pos
+		self.offset = [probe + cam for probe, cam in zip(self.probe_pos, self.cam_pos)]
+		
+
+		self.layout = [
+			# Line 1: Brightness
+			[
+				sg.Text("Brightness:", size=(20, 0), justification="left"), 
+				sg.Slider((0, 100), 0, orientation="h", size=(50, 15), key="-BRIGHTNESS SLIDER-")
+			],
+			# Line 2: Contrast
+			[
+				sg.Text("Contrast:", size=(20, 0), justification="left"),
+				sg.Slider((0, 5), 1, orientation="h", size=(50, 15), key="-CONTRAST SLIDER-")
+			],
+			# Line 3: Gaussian Blurring Kernel Size
+			[
+				sg.Text("Gaussian Blurring Kernel:", size=(20, 0), justification="left"), 
+				sg.Radio('Disable', 'Radio1', size=(10, 1), key="-KERNEL DISABLE-", default=True),
+				sg.Radio('3x3 Size', 'Radio1', size=(10, 1), key="-3x3 KERNEL SIZE-"),
+				sg.Radio('5x5 Size', 'Radio1', size=(10, 1), key="-5x5 KERNEL SIZE-"),
+				sg.Radio('9x9 Size', 'Radio1', size=(10, 1), key="-9x9 KERNEL SIZE-"),
+			],
+			[sg.Text("", size=(80, 0), justification="center")],
+			# Line 4
+			[sg.Text("Haar Cascade Object Detection", size=(80, 0), justification="center", font=('Arial', 10, 'bold'))],
+			# Line 5
+			[sg.Text("Scale Factor", size=(50, 0), justification="center"),
+			 sg.Text("Min Neighbour", size=(20, 0), justification="center"),
+			],
+			# Line 6-10: Scale Factor for Cross, Circle, Square, Triangle and Device
+			[
+				sg.Text('Cross', size=(8, 0), justification="left"),
+				sg.Slider((1, 1000), 800, orientation="h", size=(30, 15), key="-CROSS SCALE SLIDER-"),
+				sg.Slider((0, 20), 12, orientation="h", size=(30, 15), key="-CROSS NEIGHBOUR SLIDER-"),
+			],
+			[
+				sg.Text('Circle', size=(8, 0), justification="left"),
+				sg.Slider((1, 1000), 800, orientation="h", size=(30, 15), key="-CIRCLE SCALE SLIDER-"),
+				sg.Slider((0, 20), 12, orientation="h", size=(30, 15), key="-CIRCLE NEIGHBOUR SLIDER-")
+			],
+			[
+				sg.Text('Square', size=(8, 0), justification="left"),
+				sg.Slider((1, 1000), 800, orientation="h", size=(30, 15), key="-SQUARE SCALE SLIDER-"),
+				sg.Slider((0, 20), 12, orientation="h", size=(30, 15), key="-SQUARE NEIGHBOUR SLIDER-")
+			],
+			[
+				sg.Text('Triangle', size=(8, 0), justification="left"),
+				sg.Slider((1, 1000), 800, orientation="h", size=(30, 15), key="-TRIANGLE SCALE SLIDER-"),
+				sg.Slider((0, 20), 12, orientation="h", size=(30, 15), key="-TRIANGLE NEIGHBOUR SLIDER-")
+			],
+			[
+				sg.Text('Device', size=(8, 0), justification="left"),
+				sg.Slider((1, 2000), 800, orientation="h", size=(30, 15), key="-DEVICE SCALE SLIDER-"),
+				sg.Slider((0, 20), 12, orientation="h", size=(30, 15), key="-DEVICE NEIGHBOUR SLIDER-")
+			],
+			[sg.Text("", size=(80, 0), justification="center")],
+			# Line 11
+			[sg.Text("Device Contour Detection", size=(80, 0), justification="center", font=('Arial', 10, 'bold'))],
+			# Line 12: Thresholding
+			[
+				sg.Text("Thresholding Value:", size=(20, 1), justification="left"),
+				sg.Slider((0, 255), 127, orientation="h", size=(50, 15), key="-THRESHOLD SLIDER-")
+			],
+			# Line 13-14: Morphological Transformations / Noise Reduction Iterations
+			[
+				sg.Text("Iterations for Opening:", size=(20, 1), justification="left"),
+				sg.Slider((0, 5), 0, orientation="h", size=(50, 15), key="-OPENING SLIDER-")
+			],
+			[
+				sg.Text("Iterations for Closing:", size=(20, 1), justification="left"),
+				sg.Slider((0, 5),0, orientation="h", size=(50, 15), key="-CLOSING SLIDER-")
+			],
+			# Line 15
+			[sg.Button("OK", size=(10, 1))],
+		]
+
+		self.window = sg.Window("Controller", self.layout, location=(700, 100))
+
+
+	def detect(self):
+
+		data = {}
+		while (1):
+			event, values = self.window.read(timeout=0)
+
+			# Values from PySimpleGUI
+			scaleFactor_device = 1+ (int(values["-DEVICE SCALE SLIDER-"]) / 1000)
+
+			neig_device = int(values["-DEVICE NEIGHBOUR SLIDER-"])
+
+			beta = int(values["-BRIGHTNESS SLIDER-"])
+			alpha = int(values["-CONTRAST SLIDER-"])
+			threshold = int(values["-THRESHOLD SLIDER-"])
+			opening_iter = int(values["-OPENING SLIDER-"])
+			closing_iter = int(values["-CLOSING SLIDER-"])
+
+			if values["-KERNEL DISABLE-"]:
+				applyBlur = False
+			else:
+				applyBlur = True
+				if values["-3x3 KERNEL SIZE-"]:
+					blur_kernel = 3
+				elif values["-5x5 KERNEL SIZE-"]:
+					blur_kernel = 5
+				elif values["-9x9 KERNEL SIZE-"]:
+					blur_kernel = 9
+
+			# Capture the frame
+			_, frame = self.cam.read()
+			center_x = int(frame.shape[1] / 2)
+			center_y = int(frame.shape[0] / 2)
+
+			# Brightness & Contrast  
+			frame = cv2.addWeighted(frame, alpha, np.zeros(frame.shape, frame.dtype), 0, beta)
+
+			# Blurring
+			if applyBlur:
+				frame = cv2.GaussianBlur(frame,(blur_kernel,blur_kernel),0)
+
+			gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+			# Detect the object using the Cascade
+			object_device = cascade_device.detectMultiScale(image=gray_img,scaleFactor=scaleFactor_device,minNeighbors=neig_device)
+					
+			# Sorting the devices from left-to-right and top-to-bottom
+			if len(object_device) < 1:
+				pass
+			else:
+				df = pd.DataFrame(object_device)
+				df.rename(columns={0: 'x', 1: 'y', 2: 'w', 3: 'h'},inplace=True)
+				df.sort_values(by='y', ascending=True, inplace=True)
+				df.reset_index(inplace = True, drop = True)
+				df['row'] = np.ones(len(df), dtype='int64')
+				row_num = 1
+				for i in np.arange(1,len(df)): 
+					# If diff in y-coordinates > 30, assign next row (adjustable)
+					if (abs(df.loc[i,'y'] - df.loc[i-1,'y']) > 30):             
+						row_num += 1
+						df.loc[i,'row'] = row_num
+					else:
+						df.loc[i,'row'] = row_num
+				df.sort_values(by=['row','x'], ascending=[True,True], inplace=True) 
+				df.reset_index(inplace = True, drop = True)
+				object_device = df
+
+			for i in range(len(object_device)):
+				x = object_device.loc[i,'x']
+				y = object_device.loc[i,'y']
+				w = object_device.loc[i,'w']
+				h = object_device.loc[i,'h']
+				area = w*h
+				if area >= 36*36:
+					cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+					cv2.circle(frame, (int(x+(w/2)), int(y+(h/2))), 3, (0,0,255), -1)
+					cv2.putText(frame, '{}'.format(i+1), (x-8, y-4), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.6, (255,255,255), 1)
+					roi_color = frame[y:y+h, x:x+w]
+					roi_gray = cv2.cvtColor(roi_color, cv2.COLOR_BGR2GRAY)
+
+					# Morphological Transformations - Noise Removal
+					kernel = np.ones((3,3),np.uint8)
+					roi_gray = cv2.morphologyEx(roi_gray,cv2.MORPH_OPEN,kernel,iterations=opening_iter)
+					roi_gray = cv2.morphologyEx(roi_gray,cv2.MORPH_CLOSE,kernel,iterations=closing_iter)
+
+					# Image Thresholding: Inverse Binary Thresholding + the OTSU method
+					ret, thresh = cv2.threshold(roi_gray, threshold, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+					
+					# Contour Detection
+					contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+					contours = contours[1:]
+					cv2.drawContours(roi_color, contours, -1, (255,0,0), 2)
+					data['C'+str(i+1)] = [int(x+(w/2)), int(y+(h/2))]
+						
+			cv2.imshow("Result", frame)
+			cv2.imshow("Grayscale", gray_img)
+			
+			if event == "OK" or event == sg.WIN_CLOSED or event is None:
+				break
+
+			k = cv2.waitKey(5) & 0xFF
+			if k == 27:         # wait for ESC key to exit
+				break
+
+		self.cam.release()
+		cv2.destroyAllWindows()
+		self.window.close()
+
+		img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+		fig = plt.figure(figsize=(8, 6))
+		plt.imshow(img)
+		plt.xlabel('x')
+		plt.ylabel('y')
+		plt.title('Result')
+		plt.show()
+
+		fig.savefig(os.path.join(self.result_folder + '/','haarcascade.png'), dpi=fig.dpi)
+
+		# Save CSV file: Coordinates
+		data = pd.DataFrame(data)
+		data = data.T
+		data = data.copy()
+		data.rename(columns={0: 'x', 1: 'y'},inplace=True)
+		data.to_csv(os.path.join(self.result_folder + '/','from_origin_pixel.csv'))
+
+		# Save CSV file: Coordinates from Center
+		data['x'] = (data['x'] - center_x) / calibUnit
+		data['y'] = (center_y - data['y']) / calibUnit
+		data.to_csv(os.path.join(self.result_folder + '/','from_center.csv'))
+
+		# Save CSV file: Coordinates from Camera
+		data['x'] = data['x'] + cam_x 
+		data['y'] = data['y'] + cam_y
+		data.to_csv(os.path.join(self.result_folder + '/','from_cam.csv'))
+
+		# Save CSV file: Coordinates from Probe
+		data['x'] = data['x'] + probe_x
+		data['y'] = data['y'] + probe_y
+		data.to_csv(os.path.join(self.result_folder + '/','from_probe.csv'))
+
+
+
+
+
+
 		
 
 
